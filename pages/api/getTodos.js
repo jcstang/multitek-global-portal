f69547e5ef760c64e34c3907476eca28ac7af747
Jacob Stanger <devjake@outlook.com>
2021-03-04 13:03:10 -0700

    add airtable

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -5,6 +5,22 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 
 const table = base(process.env.AIRTABLE_TABLE_NAME);
 
+table.select({}).eachPage(
+  function page(records, fetchNextPage) {
+    records.forEach((record) => {
+      console.log('Retrieved', record.get('description'));
+    });
+
+    fetchNextPage();
+  },
+  function done(err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  }
+);
+
 export default (req, res) => {
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
